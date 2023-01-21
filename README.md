4b6091018b64c4d12c7e674989daea7b9f4d75ed
Joshua Mo <102877324+joshua-mo-143@users.noreply.github.com>
2023-01-21 20:32:04 +0000

    refactor: split bot into own file + readme changes

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 ## Rust Telegram Bot
 This repo is designed to showcase an example of a Telegram bot created using Rust (with the [Teloxide](https://crates.io/crates/teloxide) crate). 
+
+## Usage Pre-requisites
+This project depends on [shuttle](https://www.shuttle.rs) to be used. You'll need to sign in via Github to get an API key to use in their CLI auth so that you can deploy this project.
+
+## How to Deploy
+Clone this repository, then create a `Shuttle.toml` file where you'll set your project name and a `Secrets.toml` file where you'll set your Telegram API token to be able to use Teloxide.
+
+Then simply run `cargo shuttle project new` to initialise your project and `cargo shuttle run` to run in local or `cargo shuttle deploy` to deploy! 
---
+++ src/bot.rs
@@ -0,0 +1,62 @@
+use sqlx::PgPool;
+use teloxide::prelude::*;
+use teloxide::utils::command::BotCommands;
+pub struct BotService {
+    pub bot: Bot,
+    pub postgres: PgPool,
+}
+
+#[shuttle_service::async_trait]
+impl shuttle_service::Service for BotService {
+    async fn bind(
+        mut self: Box<Self>,
+        _addr: std::net::SocketAddr,
+    ) -> Result<(), shuttle_service::error::Error> {
+        self.start().await?;
+
+        Ok(())
+    }
+}
+
+impl BotService {
+    async fn start(&self) -> Result<(), shuttle_service::error::CustomError> {
+        Command::repl(self.bot.clone(), answer).await;
+        Ok(())
+    }
+}
+
+#[derive(BotCommands, Clone)]
+#[command(
+    rename_rule = "lowercase",
+    description = "These commands are supported:"
+)]
+enum Command {
+    #[command(description = "display this text.")]
+    Help,
+    #[command(description = "handle a username.")]
+    Username(String),
+    #[command(description = "handle a username and an age.", parse_with = "split")]
+    UsernameAndAge { username: String, age: u8 },
+}
+
+async fn answer(bot: Bot, msg: Message, cmd: Command) -> ResponseResult<()> {
+    match cmd {
+        Command::Help => {
+            bot.send_message(msg.chat.id, Command::descriptions().to_string())
+                .await?
+        }
+        Command::Username(username) => {
+            bot.send_message(msg.chat.id, format!("Your username is @{username}."))
+                .await?
+        }
+        Command::UsernameAndAge { username, age } => {
+            bot.send_message(
+                msg.chat.id,
+                format!("Your username is @{username} and age is {age}."),
+            )
+            .await?
+        }
+    };
+
+    Ok(())
+}
